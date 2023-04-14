0695c9d6d64534847caccde6207151a4575ec671
fsm13 <fomichevsm@gmail.com>
2023-04-14 19:14:44 +0700

    lesson2

--- app/src/main/java/my/newapp/roulettegame/rulescreen/RuleScreen.kt
+++ app/src/main/java/my/newapp/roulettegame/rulescreen/RuleScreen.kt
@@ -1,62 +1,98 @@
 package my.newapp.roulettegame.rulescreen
 
+import my.newapp.roulettegame.R
+import android.util.Log
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import my.newapp.roulettegame.R
+import my.newapp.roulettegame.ui.theme.Red
+import my.newapp.roulettegame.utils.ValueList
+
 
 
 @Composable
 fun RuleScreen() {
+    var rotationValue by remember {
+        mutableStateOf(0f)
+    }
+    var number by remember {
+        mutableStateOf(0)
+    }
+    val angle: Float by animateFloatAsState(
+        targetValue = rotationValue,
+        animationSpec = tween(
+            durationMillis = 2000,
+            easing = LinearOutSlowInEasing
+        ),
+        finishedListener = {
+            val index = (365f - (rotationValue % 360f)) / (360f / ValueList.list.size)
+            Log.d("MyLog", "index: ${index}")
+            number = ValueList.list[index.toInt()]
+        }
+    )
+    Log.d("MyLog", "angle: $angle")
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            modifier = Modifier.fillMaxWidth()
+            text = number.toString(),
+            modifier = Modifier
+                .fillMaxWidth()
                 .height(100.dp)
-                .wrapContentHeight()
-                .wrapContentWidth(),
-            text = "0",
-            fontWeight = FontWeight.Bold,
+                .wrapContentWidth()
+                .wrapContentHeight(),
             fontSize = 35.sp,
+            fontWeight = FontWeight.Bold,
             color = Color.White
         )
-        Box(modifier = Modifier
-            .weight(1f)
-            .fillMaxSize()){
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()){
             Image(
                 painter = painterResource(id = R.drawable.ruleta),
                 contentDescription = "Ruleta",
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .rotate(angle)
             )
             Image(
                 painter = painterResource(id = R.drawable.flecha),
-                contentDescription = "Flecha",
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.buttonColors(backgroundColor = Red),
-            modifier = Modifier.fillMaxWidth().padding(10.dp)
-        ) {
-            Text(
-                text = "Start",
-                color = White
+                contentDescription = "Arrow",
+                modifier = Modifier
+                    .fillMaxSize()
             )
         }
 
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            onClick = {
+                rotationValue = ((0..360).random().toFloat() + 720) + angle
+            },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Red)
+        ) {
+            Text(
+                text = "Start",
+                color = Color.White
+            )
+        }
     }
 }
